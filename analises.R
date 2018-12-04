15543402cd1de3fbcceafd99439917d9bf815851
fagnersutel <fagnersutel@hotmail.com>
2018-12-04 09:08:59 -0200

    Add files via upload

--- analises.R
+++ analises.R
@@ -1,18 +1,19 @@
 #install.packages('foregein')
 library(foreign)
-setwd('/Users/fagnersuteldemoura/Documents/anos/2010/')
+setwd('/Users/fsmoura/Documents/R-files/loop_by_year/anos/2010/')
 list.files()
 acidentes = read.dbf("acidentes.dbf")
 head(acidentes)
 dim(acidentes)
+dim(temp1)
 temp = acidentes[acidentes$PREDIAL1 == 0, ]
 head(temp)
 dim(temp)
 temp = acidentes[acidentes$LONGITUDE == 0, ]
 dim(temp)
-cond = read.dbf("condutores.dbf")
-dim(cond)
-head(cond)
+condutores = read.dbf("condutores.dbf")
+dim(condutores)
+head(condutores)
 vit = read.dbf("vitimas.dbf")
 dim(vit)
 head(vit[1,])
@@ -43,14 +44,32 @@ dim(cond)
 dim(vit)
 head(temp,1)
 
-loop = c(0:7)
+acidentes = NULL
+condutores = NULL
+vitimas = NULL
+loop = c(0:1)
 for(i in loop){
-  setwd(paste('/Users/fagnersuteldemoura/Documents/anos/201', i, sep = "" ))
+  setwd(paste('/Users/fsmoura/Documents/R-files/loop_by_year/anos/201', i, sep = "" ))
   print(getwd())
   print(list.files())
   acidentes1 = read.dbf("acidentes.dbf")
   acidentes = rbind(acidentes, acidentes1)
+  
+  condutores1 = read.dbf("condutores.dbf")
+  condutores = rbind(condutores, condutores1)
+  
+  vitimas1 = read.dbf("vitimas.dbf")
+  vitimas = rbind(vitimas, vitimas1)
+  
+  percent_c = (length(condutores$ID)/length(acidentes$ID))
+  percent_v = (length(condutores$ID)/length(vitimas$BOLETIM))
+  print(dim(acidentes))
+  print(paste(length(condutores$ID), percent_c, "%", sep = " " ))  
+  print(percent)
+  print(length(vitimas$BOLETIM))  
 }
+
+
 
 temp = acidentes[acidentes$PREDIAL1 == 0, ]
 head(temp)
